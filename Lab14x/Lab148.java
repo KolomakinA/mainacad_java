a03235a305dab4032019764a36e5debca98bd508
KolomakinA <kolomakin@gmail.com>
2018-05-12 11:48:31 +0300

    base

---
+++ Lab14x/Lab148.java
@@ -0,0 +1,28 @@
+package com.company.Lab14x;
+
+public class Lab148 {
+    public static void main(String[] args) {
+        byte b = 124;
+        short i = 3478;
+        int i1 = 902_378_190;
+        long l = 209_387_378_944_56L;
+
+        float v = 356.2378634F;
+        double v1 = 234364.34558563534567345345;
+
+        char c = 2384;
+
+        boolean b1 = false;
+
+
+        System.out.println((double)b);
+        System.out.println((short)v1);
+        System.out.println((long)c);
+        System.out.println((char)i);
+        System.out.println((float)i1);
+        System.out.println((byte)v);
+        System.out.println((int)l);
+        System.out.println(String.valueOf(b1));
+
+    }
+}
