34128af132b3192a5c63624c5fd61531ba172bac
KolomakinA <kolomakin@gmail.com>
2018-05-12 11:31:37 +0300

    base

---
+++ Lab14x/Lab146.java
@@ -0,0 +1,13 @@
+package com.company.Lab14x;
+
+public class Lab146 {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 83;
+
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~(a^b));
+    }
+}
