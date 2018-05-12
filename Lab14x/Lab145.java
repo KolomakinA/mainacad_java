0067bd8a3036d703ba84bc46b544c8b53af1c9a8
KolomakinA <kolomakin@gmail.com>
2018-05-12 11:20:11 +0300

    base

---
+++ Lab14x/Lab145.java
@@ -0,0 +1,11 @@
+package com.company.Lab14x;
+
+public class Lab145 {
+    public static void main(String[] args) {
+        int i = -5;
+
+        System.out.println(i << 4);
+        System.out.println(i >> 2);
+        System.out.println(i >>> 1);
+    }
+}
