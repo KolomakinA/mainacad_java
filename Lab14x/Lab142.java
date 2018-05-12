e0bd3ea1cb53a3d19bfa1cd9a8edb9554560f4e9
KolomakinA <kolomakin@gmail.com>
2018-05-12 10:38:47 +0300

    base

---
+++ Lab14x/Lab142.java
@@ -0,0 +1,23 @@
+package com.company.Lab14x;
+
+public class Lab142 {
+    public static void main(String[] args) {
+        byte bytePlusA = -128, byteplusB = 12;
+        byte byteMinusA = 99;
+        long longMinusB = 200L;
+        short shortDevideA = 32000, shortDevideB =321;
+        int intMultiplyA = 5676269;
+        float floatMultiplyB = 3.14192F;
+        char charModA = 3879;
+        double doubleModB = 387.0;
+
+
+        System.out.println(bytePlusA+byteplusB);
+        System.out.println((long)byteMinusA-longMinusB);
+        System.out.println(shortDevideA/shortDevideB);
+        System.out.println((float)intMultiplyA*floatMultiplyB);
+        System.out.println((double)charModA%doubleModB);
+
+
+    }
+}
