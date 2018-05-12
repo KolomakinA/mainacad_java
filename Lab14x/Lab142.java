157c9f08a74db23c6be4e9ea826266bec176ce97
KolomakinA <kolomakin@gmail.com>
2018-05-12 10:39:58 +0300

    typo

--- Lab14x/Lab142.java
+++ Lab14x/Lab142.java
@@ -5,7 +5,7 @@ public class Lab142 {
         byte bytePlusA = -128, byteplusB = 12;
         byte byteMinusA = 99;
         long longMinusB = 200L;
-        short shortDevideA = 32000, shortDevideB =321;
+        short shortDivideA = 32000, shortDivideB =321;
         int intMultiplyA = 5676269;
         float floatMultiplyB = 3.14192F;
         char charModA = 3879;
@@ -14,10 +14,9 @@ public class Lab142 {
 
         System.out.println(bytePlusA+byteplusB);
         System.out.println((long)byteMinusA-longMinusB);
-        System.out.println(shortDevideA/shortDevideB);
+        System.out.println(shortDivideA/shortDivideB);
         System.out.println((float)intMultiplyA*floatMultiplyB);
         System.out.println((double)charModA%doubleModB);
 
 
     }
-}
