cf511daf10f5004688b97e3cece8f42fd29f5041
KolomakinA <kolomakin@gmail.com>
2018-05-12 11:38:28 +0300

    base

---
+++ Lab14x/Lab147.java
@@ -0,0 +1,9 @@
+package com.company.Lab14x;
+
+public class Lab147 {
+    public static void main(String[] args) {
+        boolean alwaysTrue = false;
+        int i = alwaysTrue?1:0;
+        System.out.println(i);
+    }
+}
