03a3cddda41349fc5476e9d86bcca207d2b5978a
KolomakinA <kolomakin@gmail.com>
2018-05-12 10:48:10 +0300

    base

---
+++ Lab14x/Lab143.java
@@ -0,0 +1,12 @@
+package com.company.Lab14x;
+
+public class Lab143 {
+    public static void main(String[] args) {
+        boolean t = true, f = false;
+
+        System.out.println(t & f);
+        System.out.println(f | t);
+        System.out.println(! (f&t));
+        System.out.println(t ^ t);
+    }
+}
