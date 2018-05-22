6edf6238c99c57427967ffd925e88f924f58ba28
KolomakinA <kolomakin@gmail.com>
2018-05-22 21:29:29 +0300

    base

---
+++ Lab24x/Lab247.java
@@ -0,0 +1,20 @@
+public class Lab247 {
+    public static void main(String[] args) {
+        GravityCalculator gravityCalculator = new GravityCalculator();
+        System.out.println(gravityCalculator.calcDist(10));
+    }
+}
+
+public class GravityCalculator {
+    private static final double ACCELERATION = -9.81;
+
+    public double calcDist(double finalTime){
+        double distance = 0;
+        double v = 0;
+        for (int time = 0; time < finalTime; time++) {
+            distance = 0.5 * ACCELERATION * Math.pow(time, 2) + v * time + distance;
+            v = v + ACCELERATION;
+        }
+        return distance;
+    }
+}
