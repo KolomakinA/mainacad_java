46dcfe98441b5f305a4776728414a6e121650a8a
KolomakinA <kolomakin@gmail.com>
2018-05-24 09:35:10 +0300

    base

--- Lab24x/Lab241-243.java
+++ Lab24x/Lab241-243.java
@@ -1,18 +1,20 @@
-package com.company.Lab24x;
-
 import java.util.Arrays;
 
-public class Lab241 {
+public class Calculation {
     public static void main(String[] args) {
         int[] arr1 = {1,3,5};
         int[] arr2 = {2,4,8};
-        
+
         System.out.println(MyMath.findMin(arr1));
         System.out.println(MyMath.findMax(arr2));
     }
 }
 
 class MyMath{
+    final static double PI = Math.PI;
+    public static double areaOfCircle(double radius){
+        return PI*Math.pow(radius,2);
+    }
     public static int findMin(int[] arr){
         Arrays.sort(arr);
         return arr[0];
