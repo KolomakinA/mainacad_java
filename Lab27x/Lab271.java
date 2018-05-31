8ce2ef04be4374048aea1ad7922e33da692775e9
KolomakinA <kolomakin@gmail.com>
2018-05-31 08:41:58 +0300

    base

---
+++ Lab27x/Lab271.java
@@ -0,0 +1,96 @@
+package com.company.Lab27x;
+
+public class Lab271 {
+}
+
+class Device{
+    private String manufacturer;
+    private float price;
+    private String serialNumber;
+
+    public Device() {
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
+
+class Monitor extends Device {
+    private int resolutionX;
+
+    private int resolutionY;
+
+    public Monitor(String manufacturer, float price, String serialNumber, int resolutionX, int resolutionY) {
+        super.setManufacturer(manufacturer);
+        super.setPrice(price);
+        super.setSerialNumber(serialNumber);
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+    }
+
+    public int getResolutionX() {
+        return resolutionX;
+    }
+
+    public void setResolutionX(int resolutionX) {
+        this.resolutionX = resolutionX;
+    }
+
+    public int getResolutionY() {
+        return resolutionY;
+    }
+
+    public void setResolutionY(int resolutionY) {
+        this.resolutionY = resolutionY;
+    }
+}
+
+class EthernetAdapter extends Device {
+    private int speed;
+    private String mac;
+
+    public EthernetAdapter(String manufacturer, float price, String serialNumber, int speed, String mac) {
+        super.setManufacturer(manufacturer);
+        super.setPrice(price);
+        super.setSerialNumber(serialNumber);
+        this.speed = speed;
+        this.mac = mac;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+}
